0fdfc9534155a1cc1fc998ada63304148ce7a2af
freddinmg <79553156+freddinmg@users.noreply.github.com>
2025-01-23 08:07:51 -0600

    Create onOpen.js

---
+++ onOpen.js
@@ -0,0 +1,7 @@
+//Just here to give you a quick way to make your sheet. You can still run the functions themselves from the script menu.
+function onOpen() {
+ui.createMenu('Quick Page Templates')
+      .addItem('Create sheet with full editing protection', 'setRegularWarning')
+      .addItem('Create edit-protected sheet with warning cell', 'setCustomWarning')
+      .addToUi();
+}
